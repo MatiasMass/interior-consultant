086cd1bfc87f7d3ba19d4f97c45412cac7eadd53
MatiasMass <matiasmass14@gmail.com>
2023-01-26 19:54:53 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const $hamb = document.querySelector('#hamburger');
+const $navbarInner = document.querySelector('.navbar-inner');
+
+console.log($hamb)
+$hamb.addEventListener("click", function() {
+    if ($navbarInner.style.display === "none") {
+      $navbarInner.style.display = "flex";
+    } else {
+      $navbarInner.style.display = "none";
+    }
+  });
